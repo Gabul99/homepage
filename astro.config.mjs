eb3a4e51ce7b721dd73afe93dcd31c0d38953a60
Gabul99 <hclee99@kaist.ac.kr>
2024-11-12 22:20:41 +0900

    Add deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://YOURNAME.site',
-    markdown: {
-        shikiConfig: {
-            themes: {
-                light: 'snazzy-light',
-            },
-        },
+  site: "https://gabul99.github.io",
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "snazzy-light",
+      },
     },
+  },
 });
