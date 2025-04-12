9d51306d098a2e78c1f59a6f5fa4dc557f9b33d3
Gabul99 <hclee99@kaist.ac.kr>
2025-04-12 22:35:25 +0900

    Evaligner + CV update

--- src/content/news.ts
+++ src/content/news.ts
@@ -5,6 +5,15 @@ export interface News {
 
 export const newsList: News[] = [
   {
+    date: "2025-04-25",
+    description:
+      "🇯🇵 Trip to Yokohama, Japan for attending CHI 2025 (Apr 25th ~ May 2nd)",
+  },
+  {
+    date: "2025-03-28",
+    description: `🎉 My 'Evaligner' paper has been accepted to <a href="https://heal-workshop.github.io/" target="_blank">HEAL Workshop</a> at CHI 2025!`,
+  },
+  {
     date: "2025-02-03",
     description: "🔰 I've just joined CSTL as MS student.",
   },
--- src/content/publication.ts
+++ src/content/publication.ts
@@ -8,6 +8,15 @@ export interface Publication {
 
 export const publicationList: Publication[] = [
   {
+    title:
+      "Evaligner: Automatic Prompt and Criteria Refinement from User Feedback",
+    authors: ["Heechan Lee", "Tae Soo Kim", "Juho Kim"],
+    imgPath: "/assets/pub_img/evaligner.png",
+    conference:
+      "CHI 2025 Workshop on Human-centered Evaluation and Auditing of Language Models (HEAL)",
+    mainLink: "/assets/HEAL_CHI_Evaligner.pdf",
+  },
+  {
     title: "Digital Nudge Alerts: Fact-checking Generative AI Responses",
     authors: [
       "Chei Sian Lee",
