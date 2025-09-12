e8a1209e68d7eefbe0fec181f5d7dbe518ef5b09
Gabul99 <hclee99@kaist.ac.kr>
2025-09-12 22:15:43 +0900

    Update news and publication section layout

--- src/content/news.ts
+++ src/content/news.ts
@@ -5,30 +5,34 @@ export interface News {
 
 export const newsList: News[] = [
   {
-    date: "2025-07-15",
+    date: "2025-09",
+    description: "⛱️ Attend UIST 2025 at Busan, Korea. See you at Busan!",
+  },
+  {
+    date: "2025-07",
     description:
       "🇺🇸 Visit Stanford University with Prof. Joseph! (Jul 16th ~ Jul 18th) First time to visit USA!",
   },
   {
-    date: "2025-04-25",
+    date: "2025-04",
     description:
       "🇯🇵 Trip to Yokohama, Japan for attending CHI 2025 (Apr 25th ~ May 2nd)",
   },
   {
-    date: "2025-03-28",
+    date: "2025-03",
     description: `🎉 My 'Evaligner' paper has been accepted to <a href="https://heal-workshop.github.io/" target="_blank">HEAL Workshop</a> at CHI 2025!`,
   },
   {
-    date: "2025-02-03",
+    date: "2025-02",
     description: "🔰 I've just joined CSTL as MS student.",
   },
   {
-    date: "2024-12-06",
+    date: "2024-12",
     description:
       "✅ One ICADL paper is published, what I worked during NTU exchange program",
   },
   {
-    date: "2024-11-12",
+    date: "2024-11",
     description: "🚀 Website Opened. First time to use Astro.",
   },
 ];
--- src/content/publication.ts
+++ src/content/publication.ts
@@ -3,8 +3,11 @@ export interface Publication {
   authors: string[];
   conference: string;
   imgPath: string;
+  type: "conference" | "short" | "arxiv";
   mainLink?: string;
 }
+
+// Type: conference, short, arxiv
 
 export const publicationList: Publication[] = [
   {
@@ -12,6 +15,7 @@ export const publicationList: Publication[] = [
       "Evaligner: Automatic Prompt and Criteria Refinement from User Feedback",
     authors: ["Heechan Lee", "Tae Soo Kim", "Juho Kim"],
     imgPath: "/assets/pub_img/evaligner.png",
+    type: "short",
     conference:
       "CHI 2025 Workshop on Human-centered Evaluation and Auditing of Language Models (HEAL)",
     mainLink: "/assets/HEAL_CHI_Evaligner.pdf",
@@ -25,6 +29,7 @@ export const publicationList: Publication[] = [
       "Dion Hoe-Lian Goh",
     ],
     imgPath: "/assets/pub_img/icadl.png",
+    type: "short",
     conference: "ICADL 2024, Short Paper",
     mainLink: "https://doi.org/10.1007/978-981-96-0865-2_21",
   },
